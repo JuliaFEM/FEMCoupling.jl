6b82a8c34181b5dc45b5a5199fa842237e0ddae3
Ville Jämsä <vjamsa@live.fi>
2018-06-15 09:43:01 +0300

    wip new example: cylindrical beam torsion.

--- examples/cylindrical beam torsion/cylindrical_beam_torsion.jl
+++ examples/cylindrical beam torsion/cylindrical_beam_torsion.jl
@@ -11,19 +11,18 @@ a=2*L*tmax/(G*d)
 
 println(a)
 
-using JuliaFEM
+using JuliaFEM: add_elements!
 using JuliaFEM.Preprocess
 using JuliaFEM.Postprocess
 using JuliaFEM.Abaqus: create_surface_elements
 using FEMBase
-
 # read mesh
-mesh = abaqus_read_mesh("varsinolla.inp")
+mesh = abaqus_read_mesh("cylindrical_beam_torsion1.inp")
 
 # problem
-cylinder_probem = Problem(Elasticity,"varsinolla",6)
+cylinder_problem = Problem(Elasticity,"varsinolla",6)
 cylinder_element = create_elements(mesh,"Body1")
-update!(Body1, "youngs modulus", 210e3)
-update!(Body1, "poissons ratio", 0.3)
-update!(body1, "density", 7.80e-9)
+update!(cylinder_element, "youngs modulus", 210e3)
+update!(cylinder_element, "poissons ratio", 0.3)
+update!(cylinder_element, "density", 7.80e-9)
 add_elements!(cylinder_problem,cylinder_element)
