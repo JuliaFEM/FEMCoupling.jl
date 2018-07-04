3199ec198cac3806de99e73edd90adfa7852417a
Jukka Aho <ahojukka5@gmail.com>
2018-07-04 13:49:11 +0300

    Cleanup of code

--- src/distributed_coupling.jl
+++ src/distributed_coupling.jl
@@ -1,12 +1,9 @@
 # This file is a part of JuliaFEM.
 # License is MIT: see https://github.com/JuliaFEM/FEMCoupling.jl/blob/master/LICENSE
 
-
 type Coupling <: BoundaryProblem
     reference_node :: Element{Poi1}
 end
-
-export Coupling
 
 function Coupling()
     reference_node = Element(Poi1, Int64[])
@@ -29,21 +26,21 @@ function FEMBase.assemble_elements!(problem::Problem{Coupling},
                                     assembly::Assembly,
                                     elements::Vector{Element{Poi1}}, time)
 
-    dimensions=problem.dimension
+    dimensions = problem.dimension
+
     function to3d(x)
-        if length(x)==2
-            return [x;0.0]
+        if length(x) == 2
+            return [x; 0.0]
         end
         return x
     end
+
     ref_node = problem.properties.reference_node
     if length(get_connectivity(ref_node)) == 0
         error("Reference node node defined. Define reference node using add_reference_node!")
     end
     ref_node_id = first(get_connectivity(ref_node))
-    # info("Reference node id = $ref_node_id")
     X_r = to3d(first(ref_node("geometry", time)))
-    # info("Reference node geometry = $X_r")
     fe = zeros(dimensions)
     weights = Dict{Int64,Float64}()
     X_n = Dict{Int64,Vector{Float64}}()
@@ -63,18 +60,18 @@ function FEMBase.assemble_elements!(problem::Problem{Coupling},
     for n in keys(weights)
         xbar = xbar+weights[n]*X_n[n]
     end
-    r=Dict{Int64,Vector{Float64}}()
+    r = Dict{Int64,Vector{Float64}}()
     for n in keys(weights)
         r[n] = X_n[n]-xbar
     end
-            rR = X_r-xbar
-    T=zeros(3,3)
+    rR = X_r-xbar
+    T = zeros(3,3)
     for n in keys(weights)
-        T = T+weights[n]*(dot(r[n],r[n])*eye(3)-(r[n]*r[n]'))
+        T += weights[n]*(dot(r[n],r[n])*eye(3)-(r[n]*r[n]'))
     end
-    if det(T)==0
-        indz=indmin(diag(T))
-        T[indz,indz] +=1e-9
+    if det(T) == 0
+        indz = indmin(diag(T))
+        T[indz,indz] += 1e-9
     end
 
     invT = inv(T)
@@ -83,9 +80,7 @@ function FEMBase.assemble_elements!(problem::Problem{Coupling},
         fill!(fe, 0.0)
         coupling_node_id = first(get_connectivity(coupling_node))
         X_n = first(coupling_node("geometry", time))
-        # info("Coupling node id = $coupling_node_id, geometry = $X_n")
         gdofs = get_gdofs(problem, coupling_node)
-        # info("gdofs = $gdofs")
         FR = zeros(3)
         MR = zeros(3)
         for i = 1:3
@@ -97,19 +92,13 @@ function FEMBase.assemble_elements!(problem::Problem{Coupling},
             end
         end
 
-        # info("FR = $FR,MR = $MR")
-
-        # info("calculate point moment")
-
-        # display(rR)
-        # display(FR)
-        if length(rR) == 2
-            rR=[rR;0.0]
-        end
-            MRhat = MR + cross(rR,FR)
-            n = coupling_node_id
-            Fn = weights[n]*(FR+cross(invT*MRhat,r[n]))
-            fe = fe+Fn[1:dimensions]
+        MRhat = MR + cross(rR,FR)
+        n = coupling_node_id
+        Fn = weights[n]*(FR+cross(invT*MRhat,r[n]))
+        fe = fe+Fn[1:dimensions]
         add!(assembly.f, gdofs, fe)
     end
+
+    return nothing
+
 end
