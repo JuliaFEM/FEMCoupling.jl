9b257a210293aa91b160d7bdf6acb9f834c58df8
autodocs <autodocs>
2018-07-05 11:04:47 +0000

    build based on e64987f

---
+++ release-0.1/search_index.js
@@ -0,0 +1,35 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "Introduction",
+    "title": "Introduction",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#Introduction-1",
+    "page": "Introduction",
+    "title": "Introduction",
+    "category": "section",
+    "text": "Package contains coupling elements for JuliaFEM"
+},
+
+{
+    "location": "index.html#Theory-1",
+    "page": "Introduction",
+    "title": "Theory",
+    "category": "section",
+    "text": "Theory of coupling elements comes here."
+},
+
+{
+    "location": "index.html#Examples-1",
+    "page": "Introduction",
+    "title": "Examples",
+    "category": "section",
+    "text": "DocTestSetup = quote\n    using FEMBase\n    using FEMCoupling\nendSimple calculation examples of using coupling elements comes here."
+},
+
+]}
